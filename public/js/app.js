57cbd106f83a25ebf1b50930a140ec8f6f52bf01
Justin Hopkins <hopkinsju@gmail.com>
2014-02-25 21:31:01 -0600

    animated table filter

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.deliverystops']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ngAnimate', 'ui.bootstrap', 'ui.router', 'mean.system', 'mean.deliverystops']);
 
 angular.module('mean.system', []);
 angular.module('mean.deliverystops', []);
